d908e122d2e53c194cffd5788859e558c77bd435
Andrew Yoon <andrewyoon2@gmail.com>
2017-10-21 21:20:40 -0400

    restructuring

--- the_transistorized_radio_main.py
+++ the_transistorized_radio_main.py
@@ -3,13 +3,13 @@ from drone_machine import osc_interpreter
 from drone_machine import sampler_interpreter
 from drone_machine import terminal
 from drone_machine import writer
-from drone_machine.content import test
+from drone_machine.content import the_transistorized_radio as content
 
 
 out_path = 'out.wav'
 
-sampler_voices = sampler_interpreter.interpret(test.score, test.samplers)
-osc_voices = osc_interpreter.interpret(test.score, test.oscillators)
+sampler_voices = sampler_interpreter.interpret(content.score, content.samplers)
+osc_voices = osc_interpreter.interpret(content.score, content.oscillators)
 samples = rendering.render(osc_voices, sampler_voices)
 
 writer.write('out.wav', samples)
